c15e868b5979744fc9681ea4f0b4fd8d1ec3bd56
Abbas Wadiwala <AWadiwala92@gmail.com>
2018-11-13 11:27:31 +0000

    Account and Service functionality changed

--- src/main/java/domain/Service.java
+++ src/main/java/domain/Service.java
@@ -7,7 +7,8 @@ public class Service {
 	HashMap<Integer, Account> accountList = new HashMap<Integer, Account>();
 	public static int AccountCounter = 0;
 	public void addAccount(String firstName, String surname) {
-		this.accountList.put(AccountCounter, new Account(firstName,surname));
+		this.accountList.put(AccountCounter, new Account(firstName,surname, AccountCounter));
+		System.out.printf("Account was created for %s, %s with the account number of:" + AccountCounter + " \n", firstName, surname);
 		AccountCounter++;
 		
 	}
@@ -15,7 +16,7 @@ public class Service {
 	public void getAccountInfo(int accountNo) {
 		for(Integer i: accountList.keySet()) {		
 			if(i == accountNo) {
-				System.out.printf("The first name is: %s and the surname is: %s \n",accountList.get(i).getFirstName(), accountList.get(i).getFirstName());
+				System.out.printf("The first name is: %s and the surname is: %s, account number:" + accountList.get(i).getAccountNum() + "\n",accountList.get(i).getFirstName(), accountList.get(i).getFirstName() );
 			}
 			
 		}
